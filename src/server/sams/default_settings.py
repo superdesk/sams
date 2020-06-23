76cb17b9f36bcfe26304c4f4c6f663500e69f3c4
Mark Pittaway <mark.pittaway@gmail.com>
2020-06-23 18:34:09 +1000

    HACK: Add 15 seconds after starting docker for elasticsearch

--- src/server/sams/default_settings.py
+++ src/server/sams/default_settings.py
@@ -23,13 +23,13 @@ URL_PREFIX = env('URL_PREFIX', server_url.path.lstrip('/')) or ''
 MONGO_DBNAME = env('MONGO_DBNAME', 'sams')
 
 #: full mongodb connection uri, overrides ``MONGO_DBNAME`` if set
-MONGO_URI = env('MONGO_URI', 'mongodb://localhost/%s' % MONGO_DBNAME)
+MONGO_URI = env('MONGO_URI', 'mongodb://sams/%s' % MONGO_DBNAME)
 
 #: allow all mongo queries
 MONGO_QUERY_BLACKLIST = []
 
 #: elastic url
-ELASTICSEARCH_URL = env('ELASTICSEARCH_URL', 'http://localhost:9200')
+ELASTICSEARCH_URL = env('ELASTICSEARCH_URL', 'http://sams:9200')
 
 #: elastic index name
 ELASTICSEARCH_INDEX = env('ELASTICSEARCH_INDEX', 'sams')
