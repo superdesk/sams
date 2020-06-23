de46cf45432de6507c93234ac97bbd782be312a1
Tanuj Soni <tanuj.soni@sourcefabric.org>
2020-06-23 20:22:29 +0530

    SDESK-5358 flake8 errors fixed

--- src/clients/python/sams_client/client.py
+++ src/clients/python/sams_client/client.py
@@ -1,20 +1,19 @@
+"""Contains Wrapper class for the GET, POST, PATCH and DELETE methods."""
+
 import requests
 from .utils import get_base_url
 
 
 class Client(object):
-    """
-    Wrapper for the GET, POST, PATCH, DELETE methods.
-    """
+    """Wrapper for the GET, POST, PATCH, DELETE methods."""
 
     def __init__(self, configs={}):
+        """Set the base url."""
         self.base_url = get_base_url(configs)
 
     def request(self, api='/', method='get',
                 headers=None, data=None, callback=None):
-        """
-        Handles request methods.
-        """
+        """Handle request methods."""
         if callback is None:
             callback = self._default_resp_callback
         request = getattr(requests, method.lower())
@@ -25,4 +24,3 @@ class Client(object):
 
     def _default_resp_callback(self, response):
         return response
-
