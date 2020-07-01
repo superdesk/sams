d6513d81ed266effee1738ca75b38c5395694a9e
Tanuj Soni <tanuj.soni@sourcefabric.org>
2020-07-01 15:15:06 +0530

    SDESK-5359 Added docstrings for methods in utils

--- src/clients/python/sams_client/utils.py
+++ src/clients/python/sams_client/utils.py
@@ -10,10 +10,15 @@
 # at https://www.sourcefabric.org/superdesk/license
 
 from .constants import DEFAULT_HOST, DEFAULT_PORT, DEFAULT_PROTOCOL
+from typing import Dict, any, Tuple
 
 
-def load_configs(configs):
+def load_configs(configs: Dict):
     """Load host, port from configs
+
+    :param dict configs: Dictionary of configuration for server's host and port
+    :rtype: tuple
+    :return: A tuple containing host and port
     """
     host = configs.get('HOST', DEFAULT_HOST)
     port = configs.get('PORT', DEFAULT_PORT)
@@ -21,8 +26,12 @@ def load_configs(configs):
     return host, port
 
 
-def get_base_url(configs):
+def get_base_url(configs: Dict):
     """Load configs and return base url
+
+    :param dict configs: Dictionary of configuration for server's host and port
+    :rtype: str
+    :return: Base url consisting of protocol, host and port
     """
     host, port = load_configs(configs)
     return f'{DEFAULT_PROTOCOL}://{host}:{port}'
