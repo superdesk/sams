6621331318ee51ed532fcf203d2d5e75e45f9e98
Mark Pittaway <mark.pittaway@gmail.com>
2020-10-30 14:46:21 +1100

    Changes to Honcho/Gunicorn

---
+++ src/server/gunicorn_config_file_server.py
@@ -0,0 +1,16 @@
+
+import os
+import multiprocessing
+
+bind = '{}:{}'.format(
+    os.environ.get('SAMS_HOST', '0.0.0.0'),
+    os.environ.get('SAMS_PORT', '5750')
+)
+workers = int(os.environ.get('WEB_CONCURRENCY', multiprocessing.cpu_count() + 1))
+
+accesslog = '-'
+access_log_format = '%(m)s %(U)s status=%(s)s time=%(T)ss size=%(B)sb'
+
+reload = 'SAMS_RELOAD' in os.environ
+
+timeout = int(os.environ.get('WEB_TIMEOUT', 30))
